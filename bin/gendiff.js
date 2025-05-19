1fa3269e662cb09ba562e5076b65190a0d20995c
Polunina Svetlana <andj.sv@gmail.com>
2025-05-19 20:51:35 +0300

    create --help --version --description

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,13 @@
 #! /usr/bin/env node
 
 import app from '../src/cli.js'
+import { Command } from 'commander'
+
+const program = new Command
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+
+program.parse()
 
 app()
