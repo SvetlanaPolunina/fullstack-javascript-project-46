7287ece56137edcb6125a5dd656e426907a5da59
Polunina Svetlana <andj.sv@gmail.com>
2025-05-19 21:35:20 +0300

    add command arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,7 +8,12 @@ program
   .version(`${packageJson.version}`)
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
 
 program.parse()
 
+const [path1, path2] = program.args
 console.log(`format: ${program.opts().format ?? 'unknown'}`)
+console.log(`filepath1: ${path1}`)
+console.log(`filepath2: ${path2}`)
