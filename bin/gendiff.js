0fbae3a0b83b981f453395d28cc97ea85fc5d1ce
Polunina Svetlana <andj.sv@gmail.com>
2025-05-19 21:31:58 +0300

    add option --farmat

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,5 +7,8 @@ const program = new Command
 program
   .version(`${packageJson.version}`)
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
 
-  program.parse()
+program.parse()
+
+console.log(`format: ${program.opts().format ?? 'unknown'}`)
