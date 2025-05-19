fd5eb005ede07c3f5596b4e11ccecab66aade146
Polunina Svetlana <andj.sv@gmail.com>
2025-05-19 21:26:38 +0300

    add automatic package version detection in the CLI

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,11 @@
 #! /usr/bin/env node
 
 import { Command } from 'commander'
+import packageJson from '../package.json' with { type: 'json' }
 
 const program = new Command
 program
-  .version('0.0.1')
+  .version(`${packageJson.version}`)
   .description('Compares two configuration files and shows a difference.')
 
-program.parse()
+  program.parse()
