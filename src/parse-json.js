cb54d2c075ea2932ba2015de1049ea3af9ed668a
Polunina Svetlana <andj.sv@gmail.com>
2025-05-21 13:54:55 +0300

    delete parse-json.js

--- src/parse-json.js
+++
@@ -1,9 +0,0 @@
-import { readFileSync } from 'fs'
-
-const parseJson = (filepath) => {
-  const textFromFile = readFileSync(filepath, 'utf8')
-  const jsonFromFile = JSON.parse(textFromFile)
-  return jsonFromFile
-}
-
-export default parseJson
