a9cff5a673a50e0f10a6c3da35d4a53b5a888afd
Polunina Svetlana <andj.sv@gmail.com>
2025-05-21 11:45:48 +0300

    rename parse-json.js parse.js

---
+++ src/parse.js
@@ -0,0 +1,9 @@
+import { readFileSync } from 'fs'
+
+const parseJson = (filepath) => {
+  const textFromFile = readFileSync(filepath, 'utf8')
+  const jsonFromFile = JSON.parse(textFromFile)
+  return jsonFromFile
+}
+
+export default parseJson
