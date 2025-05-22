967feed374a9059e1434bc3b6e435711f9895692
Polunina Svetlana <andj.sv@gmail.com>
2025-05-22 21:02:29 +0300

    rename fixture expected-diff.txt

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -13,7 +13,7 @@ test("genDiff's main flow", () => {
   const fixturePath1 = getFixturePath('file1.json')
   const fixturePath2 = getFixturePath('file2.json')
   const recievedDiff = genDiff(fixturePath1, fixturePath2)
-  const expectedDiff = readFile('expected-file.txt').trim()
+  const expectedDiff = readFile('expected-diff.txt').trim()
 
   expect(recievedDiff).toEqual(expectedDiff)
 })
