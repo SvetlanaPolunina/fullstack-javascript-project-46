e96e6e506280ea378356f617e230f25583539c54
Polunina Svetlana <andj.sv@gmail.com>
2025-05-25 18:27:49 +0300

    add genDiffData function implementation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
-import fs from 'fs'
 import { readFileSync } from 'fs'
-import parseJson from './parsers.js'
+import { parseJson } from './parsers.js'
+import _ from 'lodash'
 
 const readFile = absoluteFilepath => readFileSync(absoluteFilepath, 'utf8').trim()
+
+const genDiffData = (oldData, newData) => {
+  const oldDataKeys = Object.keys(oldData)
+  const newDataKeys = Object.keys(newData)
+  const diffKeys = _.union(oldDataKeys, newDataKeys)
+
+  const diff = diffKeys.reduce((acc, key) => {
+    if (!Object.hasOwn(oldData, key)) {
+      return { ...acc, [key]: { status: 'added', value: newData[key] } }
+    }
+    if (!Object.hasOwn(newData, key)) {
+      return { ...acc, [key]: { status: 'removed', value: oldData[key] } }
+    }
+    if (oldData[key] !== newData[key]) {
+      return {
+        ...acc,
+        [key]: {
+          status: 'updated',
+          oldValue: oldData[key],
+          newValue: newData[key]
+        }
+      }
+    }
+
+    return { ...acc }
+  }, {})
+
+  return diff
+}
+
+const formatInternalDiff = data => {
+  return data
+}
 
 const genDiff = (absolutePathOldFile, absolutePathNewFile) => {
   const rawOldData = readFile(absolutePathOldFile)
