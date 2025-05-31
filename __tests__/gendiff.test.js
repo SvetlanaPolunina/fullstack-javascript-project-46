6209bc7332233d8eb0dc744f76f47318e84e17c7
Polunina Svetlana <andj.sv@gmail.com>
2025-05-31 19:49:19 +0300

    add test 'genDiff\'s stylish format'

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -10,10 +10,25 @@ const getFixturePath = filename => path.join(__dirname, '..', '__fixtures__', fi
 const readFixture = filename => fs.readFileSync(getFixturePath(filename), 'utf-8').trim()
 
 test.each([
+  ['plain-file1.json', 'plain-file2.json'],
+  ['plain-file1.yaml', 'plain-file2.yaml'],
+  ['plain-file1.json', 'plain-file2.yaml'],
+  ['plain-file1.yaml', 'plain-file2.json'],
+])('genDiff\'s plain files', (filename1, filename2) => {
+  const fixturePath1 = getFixturePath(filename1)
+  const fixturePath2 = getFixturePath(filename2)
+  const recievedDiff = genDiff(fixturePath1, fixturePath2)
+  const expectedDiff = readFixture('plain-expected-diff.txt')
+
+  expect(recievedDiff).toBe(expectedDiff)
+})
+
+test.each([
   ['file1.json', 'file2.json'],
   ['file1.yaml', 'file2.yaml'],
   ['file1.json', 'file2.yaml'],
-])('genDiff\'s main flow', (filename1, filename2) => {
+  ['file1.yaml', 'file2.json'],
+])('genDiff\'s stylish format', (filename1, filename2) => {
   const fixturePath1 = getFixturePath(filename1)
   const fixturePath2 = getFixturePath(filename2)
   const recievedDiff = genDiff(fixturePath1, fixturePath2)
