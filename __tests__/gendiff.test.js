f9d7fe99fec6fd7a6ea18fb93b99faa954812bbc
Polunina Svetlana <andj.sv@gmail.com>
2025-06-01 17:48:31 +0300

    Revert "fix tests" because toString() is not needed while reading file with utf-8
    This reverts commit 50e78a314a73f684a7cdc4e016ce3c6d8019c4ee.

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const getFixturePath = filename => path.join(__dirname, '..', '__fixtures__', filename)
-const readFixture = filename => fs.readFileSync(getFixturePath(filename), 'utf-8').toString()
+const readFixture = filename => fs.readFileSync(getFixturePath(filename), 'utf-8').trim()
 
 test.each([
   ['plain-file1.json', 'plain-file2.json'],
