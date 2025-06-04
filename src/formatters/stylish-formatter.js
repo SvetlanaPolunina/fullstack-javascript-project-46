749394551f00bfa01aae6778f78906b71b336ce4
Polunina Svetlana <andj.sv@gmail.com>
2025-06-04 21:40:48 +0300

    feat: фвв stalish-farmatter

--- src/formatters/stylish-formatter.js
+++ src/formatters/stylish-formatter.js
@@ -1,28 +1,59 @@
 import _ from 'lodash'
+import { isObj } from '../index.js'
 
-const genDiffLine = (key, diffData) => {
-  const status = diffData[key].status
-  switch (status) {
-    case 'added':
-      return `  + ${key}: ${diffData[key].value}`
-    case 'removed':
-      return `  - ${key}: ${diffData[key].value}`
-    case 'unchanged':
-      return `    ${key}: ${diffData[key].value}`
-    case 'updated':
-      return `  - ${key}: ${diffData[key].oldValue}\n  + ${key}: ${diffData[key].newValue}`
+const shiftSymbol = ' '
+const shiftAmount = 4
+const shiftLeft = 2
+
+const formatObject = (obj, depth) => {
+  const keys = Object.keys(obj)
+  const sortedKey = _.sortBy(keys)
+  const keyShift = `${shiftSymbol.repeat(depth * shiftAmount)}`
+  const braceShift = `${shiftSymbol.repeat((depth - 1) * shiftAmount)}`
+
+  const formatedKeys = sortedKey.map((key) => {
+    if (!isObj(obj[key])) {
+      return `${keyShift}${key}: ${obj[key]}`
+    }
+    return `${keyShift}${key}: ${formatObject(obj[key], depth + 1)}`
+  })
+
+  return `{\n${formatedKeys.join('\n')}\n${braceShift}}`
+}
+
+const formatValue = (value, depth) => {
+  if (isObj(value)) {
+    return formatObject(value, depth)
   }
+  return value
 }
 
 const formatStylish = (data) => {
-  const keys = Object.keys(data)
-  const sortedKeys = _.sortBy(keys)
+  const iter = (tree, depth) => {
+    const keyShift = `${shiftSymbol.repeat(depth * shiftAmount - shiftLeft)}`
+    const braceShift = `${shiftSymbol.repeat((depth - 1) * shiftAmount)}`
+    const diffs = tree.diff
+    const formatedDiffs = diffs.map((key) => {
+      switch (key.status) {
+        case 'added':
+          return `${keyShift}+ ${key.name}: ${formatValue(key.value, depth + 1)}`
+        case 'removed':
+          return `${keyShift}- ${key.name}: ${formatValue(key.value, depth + 1)}`
+        case 'unchanged':
+          return `${keyShift}  ${key.name}: ${formatValue(key.value, depth + 1)}`
+        case 'updated':
+          return `${keyShift}- ${key.name}: ${formatValue(key.oldValue, depth + 1)}\n${keyShift}+ ${key.name}: ${formatValue(key.newValue, depth + 1)}`
+        case 'nested':
+          return `${keyShift}  ${key.name}: ${iter(key, depth + 1)}`
+        default:
+          throw new Error(`Unknown key status ${key.status}`)
+      }
+    })
 
-  const formatedDiff = sortedKeys
-    .reduce((acc, key) => [...acc, genDiffLine(key, data)], [])
-    .join('\n')
+    return `{\n${formatedDiffs.join('\n')}\n${braceShift}}`
+  }
 
-  return `{\n${formatedDiff}\n}`
+  return iter(data, 1)
 }
 
 export default formatStylish
