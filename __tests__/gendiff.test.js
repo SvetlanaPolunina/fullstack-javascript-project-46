f41136839c3ba8b5aedc98c0cb6c50df8d9f7e1b
Polunina Svetlana <andj.sv@gmail.com>
2025-06-05 04:50:59 +0300

    tests: rename nested fixtures

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,38 +9,25 @@ const __dirname = path.dirname(__filename)
 const getFixturePath = filename => path.join(__dirname, '..', '__fixtures__', filename)
 const readFixture = filename => fs.readFileSync(getFixturePath(filename), 'utf-8').trim()
 
-test.each([
-  ['plain-file1.json', 'plain-file2.json'],
-  ['plain-file1.yaml', 'plain-file2.yaml'],
-])('genDiff\'s plain files stylish format', (filename1, filename2) => {
+const files = [
+  ['file1.json', 'file2.json'],
+  ['file1.yaml', 'file2.yaml'],
+]
+
+test.each(files)('genDiff\'s stylish format', (filename1, filename2) => {
   const fixturePath1 = getFixturePath(filename1)
   const fixturePath2 = getFixturePath(filename2)
-  const recievedDiff = genDiff(fixturePath1, fixturePath2, 'stylish')
-  const expectedDiff = readFixture('plain-expected-diff.txt')
+  const actualDiff = genDiff(fixturePath1, fixturePath2, 'stylish')
+  const expectedDiff = readFixture('expected-stylish-diff.txt')
 
-  expect(recievedDiff).toBe(expectedDiff)
+  expect(actualDiff).toBe(expectedDiff)
 })
 
-test.each([
-  ['nested-file1.json', 'nested-file2.json'],
-  ['nested-file1.yaml', 'nested-file2.yaml'],
-])('genDiff\'s nested files stylish format', (filename1, filename2) => {
+test.each(files)('genDiff\'s plain format', (filename1, filename2) => {
   const fixturePath1 = getFixturePath(filename1)
   const fixturePath2 = getFixturePath(filename2)
-  const recievedDiff = genDiff(fixturePath1, fixturePath2, 'stylish')
-  const expectedDiff = readFixture('nested-expected-diff.txt')
-
-  expect(recievedDiff).toBe(expectedDiff)
-})
-
-test.each([
-  ['nested-file1.json', 'nested-file2.json'],
-  ['nested-file1.yaml', 'nested-file2.yaml'],
-])('genDiff\'s nested files plain format', (filename1, filename2) => {
-  const fixturePath1 = getFixturePath(filename1)
-  const fixturePath2 = getFixturePath(filename2)
-  const recievedDiff = genDiff(fixturePath1, fixturePath2, 'plain')
+  const actualDiff = genDiff(fixturePath1, fixturePath2, 'plain')
   const expectedDiff = readFixture('expected-plain-diff.txt')
 
-  expect(recievedDiff).toBe(expectedDiff)
+  expect(actualDiff).toBe(expectedDiff)
 })
