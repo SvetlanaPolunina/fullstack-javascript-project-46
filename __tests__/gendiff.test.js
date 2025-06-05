c90d443da9caac12d26a98d1238df2592c3319b5
Polunina Svetlana <andj.sv@gmail.com>
2025-06-05 10:21:36 +0300

    test: refactor tests usuin .each

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,34 +9,17 @@ const __dirname = path.dirname(__filename)
 const getFixturePath = filename => path.join(__dirname, '..', '__fixtures__', filename)
 const readFixture = filename => fs.readFileSync(getFixturePath(filename), 'utf-8').trim()
 
-const files = [
-  ['file1.json', 'file2.json'],
-  ['file1.yaml', 'file2.yaml'],
-]
+const diffFormats = ['stylish', 'plain', 'json']
+const fileFormats = ['json', 'yaml', 'yml']
 
-test.each(files)('genDiff\'s stylish format', (filename1, filename2) => {
-  const fixturePath1 = getFixturePath(filename1)
-  const fixturePath2 = getFixturePath(filename2)
-  const actualDiff = genDiff(fixturePath1, fixturePath2, 'stylish')
-  const expectedDiff = readFixture('expected-stylish-diff.txt')
+describe.each(diffFormats)('genDiff %s diff format', (diffFormat) => {
+  const expectedDiff = readFixture(`expected-${diffFormat}-diff.txt`)
 
-  expect(actualDiff).toBe(expectedDiff)
-})
+  test.each(fileFormats)('gendiff %s files', (fileFormat) => {
+    const fixturePath1 = getFixturePath(`file1.${fileFormat}`)
+    const fixturePath2 = getFixturePath(`file2.${fileFormat}`)
+    const actualDiff = genDiff(fixturePath1, fixturePath2, `${diffFormat}`)
 
-test.each(files)('genDiff\'s plain format', (filename1, filename2) => {
-  const fixturePath1 = getFixturePath(filename1)
-  const fixturePath2 = getFixturePath(filename2)
-  const actualDiff = genDiff(fixturePath1, fixturePath2, 'plain')
-  const expectedDiff = readFixture('expected-plain-diff.txt')
-
-  expect(actualDiff).toBe(expectedDiff)
-})
-
-test.each(files)('genDiff\'s json format', (filename1, filename2) => {
-  const fixturePath1 = getFixturePath(filename1)
-  const fixturePath2 = getFixturePath(filename2)
-  const actualDiff = genDiff(fixturePath1, fixturePath2, 'json')
-  const expectedDiff = readFixture('expected-json-diff.txt')
-
-  expect(actualDiff).toBe(expectedDiff)
+    expect(actualDiff).toBe(expectedDiff)
+  })
 })
