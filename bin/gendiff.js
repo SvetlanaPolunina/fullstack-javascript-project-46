2e77d80221bd38b479c6ec910152eb05f54f6537
Polunina Svetlana <andj.sv@gmail.com>
2025-06-05 19:15:13 +0300

    move program from separate module to entry point script

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,20 @@
 #! /usr/bin/env node
 
-import cli from '../src/cli.js'
+import { Command } from 'commander'
+import packageJson from '../package.json' with { type: 'json' }
+import genDiff from './index.js'
 
-cli()
+const program = new Command()
+
+program
+  .version(`${packageJson.version}`)
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action((filepath1, filepath2) => {
+    const diff = genDiff(filepath1, filepath2, program.opts().format)
+    console.log(diff)
+  })
+
+program.parse()
