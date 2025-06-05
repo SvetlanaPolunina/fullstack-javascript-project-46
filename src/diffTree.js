f7c1906487ea0873a8b9aea49eca4e7715f07fee
Polunina Svetlana <andj.sv@gmail.com>
2025-06-05 18:39:23 +0300

    ref: move denDiffTree function into a separate module

---
+++ src/diffTree.js
@@ -0,0 +1,40 @@
+import _ from 'lodash'
+
+const genDiffs = (data1, data2) => {
+  const data1Keys = Object.keys(data1)
+  const data2Keys = Object.keys(data2)
+  const diffKeys = _.union(data1Keys, data2Keys)
+  const sortedDiffKeys = _.sortBy(diffKeys)
+
+  const diffs = sortedDiffKeys.map((key) => {
+    const oldValue = structuredClone(data1[key])
+    const newValue = structuredClone(data2[key])
+
+    if (!Object.hasOwn(data1, key)) {
+      return { name: key, status: 'added', value: newValue }
+    }
+    if (!Object.hasOwn(data2, key)) {
+      return { name: key, status: 'removed', value: oldValue }
+    }
+    if (_.isPlainObject(oldValue) && _.isPlainObject(newValue)) {
+      return { name: key, status: 'nested', diff: genDiffs(oldValue, newValue) }
+    }
+    if (oldValue === newValue) {
+      return { name: key, status: 'unchanged', value: oldValue }
+    }
+    return { name: key, status: 'updated', oldValue, newValue }
+  })
+
+  return diffs
+}
+
+const genDiffTree = (data1, data2) => {
+  const diffTree = {
+    name: `a difference between two configuration files`,
+    diff: genDiffs(data1, data2),
+  }
+
+  return diffTree
+}
+
+export default genDiffTree
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { readFileSync } from 'fs'
 import path from 'path'
-import _ from 'lodash'
 import getParser from './parsers.js'
 import getFormatter from './formatters/index.js'
+import genDiffTree from './diffTree.js'
 
 const getFileFormat = filepath => path.extname(filepath).split('.').pop()
 
@@ -21,43 +21,12 @@ const getData = (filepath) => {
   return parsedData
 }
 
-const genDiffData = (data1, data2) => {
-  const data1Keys = Object.keys(data1)
-  const data2Keys = Object.keys(data2)
-  const diffKeys = _.union(data1Keys, data2Keys)
-  const sortedDiffKeys = _.sortBy(diffKeys)
-
-  const diffs = sortedDiffKeys.map((key) => {
-    const oldValue = structuredClone(data1[key])
-    const newValue = structuredClone(data2[key])
-
-    if (!Object.hasOwn(data1, key)) {
-      return { name: key, status: 'added', value: newValue }
-    }
-    if (!Object.hasOwn(data2, key)) {
-      return { name: key, status: 'removed', value: oldValue }
-    }
-    if (_.isPlainObject(oldValue) && _.isPlainObject(newValue)) {
-      return { name: key, status: 'nested', diff: genDiffData(oldValue, newValue) }
-    }
-    if (oldValue === newValue) {
-      return { name: key, status: 'unchanged', value: oldValue }
-    }
-    return { name: key, status: 'updated', oldValue, newValue }
-  })
-
-  return diffs
-}
-
 const genDiff = (filepath1, filepath2, formatName) => {
   const data1 = getData(filepath1)
   const data2 = getData(filepath2)
   const formatDiff = getFormatter(formatName)
 
-  const diffTree = {
-    name: `a difference between two configuration files`,
-    diff: genDiffData(data1, data2),
-  }
+  const diffTree = genDiffTree(data1, data2)
   const formatedDiff = formatDiff(diffTree)
 
   return formatedDiff
