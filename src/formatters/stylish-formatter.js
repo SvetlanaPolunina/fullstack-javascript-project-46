173266a8815fd222a1d21a5b37d36c3cad8789c7
Polunina Svetlana <andj.sv@gmail.com>
2025-06-06 05:24:05 +0300

    ref: add key-based dispatch for format stylish

--- src/formatters/stylish-formatter.js
+++ src/formatters/stylish-formatter.js
@@ -1,56 +1,78 @@
 import _ from 'lodash'
 
-const shiftSymbol = ' '
-const shiftAmount = 4
-const shiftLeft = 2
+const depthStart = 1
 
-const formatValue = (value, depth) => {
-  if (!_.isPlainObject(value)) {
-    return value
+const shiftMap = {
+  symbol: ' ',
+  amount: 4,
+}
+
+const diffShiftLeft = 2
+
+const getKeyShift = (depth, map, shiftLeft = 0) => {
+  return `${map.symbol.repeat(depth * map.amount - shiftLeft)}`
+}
+
+const getBraceShift = (depth, shiftMap) => {
+  return `${shiftMap.symbol.repeat((depth - 1) * shiftMap.amount)}`
+}
+
+const formatData = (data, depth) => {
+  if (!_.isPlainObject(data)) {
+    return data
   }
+  const keyShift = getKeyShift(depth, shiftMap)
+  const braceShift = getBraceShift(depth, shiftMap)
 
-  const keys = Object.keys(value)
+  const keys = Object.keys(data)
   const sortedKey = _.sortBy(keys)
-  const keyShift = `${shiftSymbol.repeat(depth * shiftAmount)}`
-  const braceShift = `${shiftSymbol.repeat((depth - 1) * shiftAmount)}`
 
   const formatedKeys = sortedKey.map((key) => {
-    if (!_.isPlainObject(value[key])) {
-      return `${keyShift}${key}: ${value[key]}`
+    if (!_.isPlainObject(data[key])) {
+      const formatedKey = `${keyShift}${key}: ${data[key]}`
+      return formatedKey
     }
-    return `${keyShift}${key}: ${formatValue(value[key], depth + 1)}`
+
+    const formatedNestedKey = `${keyShift}${key}: ${formatData(data[key], depth + 1)}`
+    return formatedNestedKey
   })
 
-  return `{\n${formatedKeys.join('\n')}\n${braceShift}}`
+  const formatedData = formatedKeys.join('\n')
+
+  return `{\n${formatedData}\n${braceShift}}`
 }
 
 const formatStylish = (data) => {
   const iter = (tree, depth) => {
-    const keyShift = `${shiftSymbol.repeat(depth * shiftAmount - shiftLeft)}`
-    const braceShift = `${shiftSymbol.repeat((depth - 1) * shiftAmount)}`
+    const keyShift = getKeyShift(depth, shiftMap, diffShiftLeft)
+    const braceShift = getBraceShift(depth, shiftMap)
+
     const diffs = tree.children
 
     const formatedDiffs = diffs.map((key) => {
-      switch (key.status) {
-        case 'added':
-          return `${keyShift}+ ${key.name}: ${formatValue(key.value, depth + 1)}`
-        case 'removed':
-          return `${keyShift}- ${key.name}: ${formatValue(key.value, depth + 1)}`
-        case 'unchanged':
-          return `${keyShift}  ${key.name}: ${formatValue(key.value, depth + 1)}`
-        case 'updated':
-          return `${keyShift}- ${key.name}: ${formatValue(key.oldValue, depth + 1)}\n${keyShift}+ ${key.name}: ${formatValue(key.newValue, depth + 1)}`
-        case 'nested':
-          return `${keyShift}  ${key.name}: ${iter(key, depth + 1)}`
-        default:
-          throw new Error(`Unknown key status ${key.status}`)
+      const statusMap = {
+        added: key => `${keyShift}+ ${key.name}: ${formatData(key.value, depth + 1)}`,
+        removed: key => `${keyShift}- ${key.name}: ${formatData(key.value, depth + 1)}`,
+        unchanged: key => `${keyShift}  ${key.name}: ${formatData(key.value, depth + 1)}`,
+        updated: key => `${keyShift}- ${key.name}: ${formatData(key.oldValue, depth + 1)}\n${keyShift}+ ${key.name}: ${formatData(key.newValue, depth + 1)}`,
+        nested: key => `${keyShift}  ${key.name}: ${iter(key, depth + 1)}`,
       }
+      const getDiffLineCreater = (status, map) => {
+        if (!Object.hasOwn(map, status)) {
+          throw new Error(`Unknown status: ${status}`)
+        }
+        return map[status]
+      }
+
+      const genDiffLine = getDiffLineCreater(key.status, statusMap)
+      return genDiffLine(key)
     })
 
-    return `{\n${formatedDiffs.join('\n')}\n${braceShift}}`
+    const formatedDiff = formatedDiffs.join('\n')
+    return `{\n${formatedDiff}\n${braceShift}}`
   }
 
-  return iter(data, 1)
+  return iter(data, depthStart)
 }
 
 export default formatStylish
